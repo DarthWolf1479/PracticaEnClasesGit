66062249f73ca8d89e37691bacfff33611547188
Mauricio Reynolds <Mauricio Reynolds@DESKTOP-81NKNI8>
2017-06-12 08:49:38 -0400

    Motocicleta actualizada

--- PracticaEnClases/src/Motocicleta.java
+++ PracticaEnClases/src/Motocicleta.java
@@ -5,4 +5,20 @@
  */
 public class Motocicleta {
 
+	public String marca;
+	public String modelo;
+	public int año;
+	public double cilindrada;
+	
+	public void Motocicleta(String marca, String modelo, int año, double cilindrada){
+		this.marca=marca;
+		this.modelo= modelo;
+		this.año=año;
+		this.cilindrada = cilindrada;
+	}
+	public String Mostrar(){
+		String mostrar;
+		mostrar="Es una moto de marca "+ marca + " modelo "+ modelo + " del año " + año + " de cilindrada " + cilindrada ;
+		return mostrar;
+	}
 }

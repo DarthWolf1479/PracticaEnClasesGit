207c9fb21948c323a4e344401d509d65d697899a
DarthWolf1479 <lopez.wolf.98@gmail.com>
2017-06-12 09:03:55 -0400

    Merge Master

--- PracticaEnClases/src/Motocicleta.java
+++ PracticaEnClases/src/Motocicleta.java
@@ -9,16 +9,18 @@ public class Motocicleta {
 	public String modelo;
 	public int año;
 	public double cilindrada;
-	
-	public void Motocicleta(String marca, String modelo, int año, double cilindrada){
-		this.marca=marca;
-		this.modelo= modelo;
-		this.año=año;
+
+	public void Motocicleta(String marca, String modelo, int año, double cilindrada) {
+		this.marca = marca;
+		this.modelo = modelo;
+		this.año = año;
 		this.cilindrada = cilindrada;
 	}
-	public String Mostrar(){
+
+	public String Mostrar() {
 		String mostrar;
-		mostrar="Es una moto de marca "+ marca + " modelo "+ modelo + " del año " + año + " de cilindrada " + cilindrada ;
+		mostrar = "Es una moto de marca " + marca + " modelo " + modelo
+				+ " del año " + año + " de cilindrada " + cilindrada;
 		return mostrar;
 	}
 }
